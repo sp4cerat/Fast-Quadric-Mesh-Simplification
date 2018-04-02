f556bae163b79eacac82d8eac5c40a92a6c33cbd
TigerHuang <ynotstartups@gmail.com>
2018-04-02 23:05:59 +0100

    add simplify button

--- worker.js
+++ worker.js
@@ -1,7 +1,9 @@
 self.addEventListener('message', function(e) {
-    var file = e.data.blob;
-    var percentage = e.data.percentage;
-    simplify(file, percentage);
+    const file = e.data.blob;
+    const percentage = e.data.percentage;
+    const simplify_name = e.data.simplify_name;
+
+    simplify(file, percentage, simplify_name);
 }, false);
 
 var Module = {
@@ -12,9 +14,9 @@ var Module = {
 
 self.importScripts("a.out.js");
 
-function simplify(file, percentage) {
+function simplify(file, percentage, simplify_name) {
 
-    console.log("in worker process", file, percentage);
+    console.log("in worker process", file, percentage, simplify_name);
 
     var filename = file.name;
     var fr = new FileReader();
@@ -22,16 +24,20 @@ function simplify(file, percentage) {
 
     fr. onloadend = function (e) {
         var data = new Uint8Array(fr.result);
-        Module['FS_createDataFile'](".", filename, data, true, true);
+        try {
+            Module.FS_stat(filename);
+        } catch (err) {
+            Module.FS_createDataFile(".", filename, data, true, true);
+        }
+
         Module.ccall("simplify", // c function name
                 undefined, // return
-                ["string", "number"], // param
-                [filename, percentage]
+                ["string", "number", "string"], // param
+                [filename, percentage, simplify_name]
         );
-        const simplify_filename = 'simplify.stl';
-        let out_bin = Module['FS_readFile'](simplify_filename);
+        let out_bin = Module.FS_readFile(simplify_name);
         // sla should work for binary stl
         let file = new Blob([out_bin], {type: 'application/sla'});
-        self.postMessage({"blob":file, "original_name":filename});
+        self.postMessage({"blob":file});
     }
 }
